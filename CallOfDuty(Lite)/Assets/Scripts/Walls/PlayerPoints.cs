f5334a20f6eedc265b947506e22f21ef961762ec
KrezDev <116980334+KrezDev@users.noreply.github.com>
2024-08-23 14:14:36 +0300

    DevLog #7

--- CallOfDuty(Lite)/Assets/Scripts/Walls/PlayerPoints.cs
+++ CallOfDuty(Lite)/Assets/Scripts/Walls/PlayerPoints.cs
@@ -1,13 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class PlayerPoints : MonoBehaviour
 {
     public int currentPoints = 0;
+    public TMP_Text pointsText;
+
+    void Start()
+    {
+        UpdatePointsText();
+    }
 
     public void AddPoints(int amount)
     {
         currentPoints += amount;
         Debug.Log("Points added: " + amount);
+        UpdatePointsText();
     }
 
     public void SpendPoints(int amount)
@@ -16,10 +24,16 @@ public class PlayerPoints : MonoBehaviour
         {
             currentPoints -= amount;
             Debug.Log("Points spent: " + amount);
+            UpdatePointsText();
         }
         else
         {
             Debug.Log("Not enough points to spend.");
         }
+    }
+
+    void UpdatePointsText()
+    {
+        pointsText.text = "Your Points: " + currentPoints.ToString();
     }
 }
